4125e83f4892178fcffbbb1ff08e8aff6b6ad225
quol <kuolamol77@gmail.com>
2025-05-04 15:38:59 +0300

    added images and more content to pages

--- script.js
+++
@@ -1,6 +0,0 @@
- // Simple JS interactivity
- document.querySelectorAll('article').forEach(item => {
-    item.addEventListener('click', () => {
-      alert('Product clicked: ' + item.querySelector('h3').textContent);
-    });
-  });
